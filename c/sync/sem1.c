a3e5978424cfe0d81af56fd7ce543c002e8f8b82
mayank <m.charly3015@gmail.com>
2016-11-21 03:57:55 +0530

    sem test1

--- c/sync/sem1.c
+++ c/sync/sem1.c
@@ -3,8 +3,20 @@
 #include <pthread.h>
 #include <semaphore.h>
 
+pthread_t tid[3];
+sem_t mutex1,mutex2;
+sem_init(&mutex1,0,1);
+sem_init(&mutex2,0,1);
+
+void *counter(void *value)
+{
+
+}
+
 
 int main(int argc, char const *argv[]) {
-  printf("semaphore example\n");
+  printf("Thread sync using semaphore example\n");
+
+
   return 0;
 }
