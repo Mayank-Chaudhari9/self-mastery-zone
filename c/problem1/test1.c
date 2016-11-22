ac743d6b179fd3f9f4000d4495fbc4d6fb76bfdb
mayank <m.charly3015@gmail.com>
2016-11-23 00:10:48 +0530

    prob1

--- c/problem1/test1.c
+++ c/problem1/test1.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
 
 int main(int argc, char const *argv[]) {
-  
+
   printf("first integration  test \n");
   return 0;
 }
