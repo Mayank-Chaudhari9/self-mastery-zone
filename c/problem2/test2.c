baa7bdf8db00ec208bdc86c1bb7adc7a40cc8828
mayank <m.charly3015@gmail.com>
2016-11-25 04:03:08 +0530

    added smokers to queue

--- c/problem2/test2.c
+++ c/problem2/test2.c
@@ -1,27 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.c>
+#include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
 #define SMOKERS 5
 
-pthread_t smoker[SMOKERS];
+pthread_t smokers[SMOKERS],agent;
+sem_t distributer,paper_match,tobacco_match,paper_tobacco;
 
-void* smoker(void *arg)
+//---------------------------------- Queue logic ------------------------//
+struct node
 {
+  pthread_t id;
+  struct node *link;
+}*front,*rear;
 
+void insert(pthread_t id)
+{
+  struct node *temp;
+  temp=(struct node*)malloc(sizeof(struct node));
+  temp->id=id;
+  temp->link=NULL;
+  if(rear ==NULL)
+  {
+    front=rear=temp;
+  }
+  else
+  {
+      rear->link=temp;
+      rear=temp;
+  }
 }
-void* agent(void *arg)
-{
 
+void delete()
+{
+  struct node *temp;
+   temp=front;
+   if (front==NULL)
+   {
+     printf("queue is empty\n");
+     front=rear=NULL;
+   }
+   else
+   {
+      printf("removed element is %lu\n",front->id);
+      front=front->link;
+      free(temp);
+   }
+}
+
+void queue_size()
+{
+    struct node *temp;
+
+    temp = front;
+    int cnt = 0;
+    if (front  ==  NULL)
+    {
+        printf(" queue empty \n");
+    }
+    while (temp)
+    {
+        printf("%lu  ", temp->id);
+        temp = temp->link;
+        cnt++;
+    }
+    printf("********* size of queue is %d ******** \n", cnt);
+}
+//---------------------------------------------Queue ends here-----------------
+
+void* smoker(void* arg)
+{
+  pthread_t tid=pthread_self();
+  printf("Smoker %lu is smoking \t\n",tid);
+  insert(tid);
+}
+
+
+
+void* table(void *arg)
+{
+  int i=0;
+  pthread_t id =pthread_self();
+
+  if(pthread_equal(id,agent))
+  {
+    while(1)
+      {
+        sem_wait(&distributer);
+        printf("Agent is distributing \n" );
+        i=rand()%3;
+        if(i==0)
+          sem_post(&paper_match);
+        if(i==1)
+          sem_post(&paper_tobacco);
+        if(i==2)
+          sem_post(&tobacco_match);
+      }
+  }
 }
 
 
 int main()
 {
-  int i;
-  for(i=0;i<SMOKERS;i++;)
-    pthread_create(&smoker[i],NULL,&smoker,NULL);
+  int i=0,smoker_id[SMOKERS];
 
+  sem_init(&distributer,0,1);
+  sem_init(&paper_match,0,0);
+  sem_init(&paper_tobacco,0,0);
+  sem_init(&tobacco_match,0,0);
+
+
+  pthread_create(&agent,NULL,&table,NULL);
+
+  for(i=0;i<SMOKERS;i++)
+  {
+    //smoker_id[i];
+    //pthread_create(&smokers[i],NULL,&smoker,&smoker_id[i]);
+    pthread_create(&smokers[i],NULL,&smoker,NULL);
+  }
+
+  for(i=0;i<SMOKERS;i++)
+    pthread_join(smokers[i],NULL);
+
+  queue_size();
   return 0;
 }
