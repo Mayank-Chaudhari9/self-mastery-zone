ea17f4ac468cec69b3c51cdf5b86f16ea7b3c6df
mayank <m.charly3015@gmail.com>
2016-11-25 02:25:54 +0530

    test 1

---
+++ c/problem2/test2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.c>
+#include <semaphore.h>
+#include <unistd.h>
+
+void* smoker(void *arg)
+{
+
+}
+void* agent(void *arg)
+{
+
+}
+
+
+
+
+
+
+
+int main()
