a25357e42d9231282b889b83a7ed21298aee1a12
Mayank-Chaudhari9 <m.charly3015@gmail.com>
2017-06-21 23:54:21 +0530

    new pc

---
+++ cpp/problems/postfix_evl.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+
+
+
+using namespace std;
+
+stack <string> post;
+
+
+int main()
+{
+
+    string s="mayank";
+    s ="sdssdf";
+    cout << s <<endl;
+    s[1] = 'b';
+    cout << s<< endl;
+
+
+
+
+    return 0;
+}
