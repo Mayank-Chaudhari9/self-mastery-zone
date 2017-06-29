1297cb98eed5148164927b146af8f029734dfdbe
Mayank-Chaudhari9 <m.charly3015@gmail.com>
2017-06-29 23:50:43 +0530

    dp

--- DP/0-1Kanpsack/knapsac.cpp
+++ DP/0-1Kanpsack/knapsac.cpp
@@ -20,6 +20,28 @@ int recursive(int W, int wt[], int val[], int n)
 }
 
 
+int dp(int W, int wt[], int val[], int n)
+{
+	int i,w;
+
+	int k[n+1][W+1];
+
+	for(i=0; i<=n; i++)
+	{
+		for(w=0; w<=W; w++)
+			{
+
+				if (i==0 || w==0)
+					k[i][w]= 0;
+				else if(wt[i-1] <= W)
+					k[i][w] =max(val[i-1] + k[i-1][w-wt[i-1]], k[i-1][w]);
+				else
+					k[i][w] = k[i-1][w];
+			}
+	}
+
+	return k[n][W];
+}
 
 
 
@@ -32,6 +54,10 @@ int main(int argc, char const *argv[])
 	int W= 50;
 	int n= sizeof(val)/sizeof(int);
 	cout << recursive(W, wt, val, n);
+	cout << endl;
+	cout << dp(W, wt, val,n);
+
+	cout << endl;
 
 
 
