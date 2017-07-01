be31a785fe7152e63a40c25a4290b8e76fe74963
mayank <m.charly3015@gmail.com>
2017-07-02 00:19:12 +0530

    count_words

--- strings/count_words_present.cpp
+++ strings/count_words_present.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 
-int count_words(string words[], string str)
+int count_words(vector<string> v, string str)
 {
     map<string ,int> m;
+
+    //cout << sizeof(words);
 
     istringstream s1(str);
     string s;
@@ -14,25 +16,33 @@ int count_words(string words[], string str)
       //cout << s<< endl;
       m[s]++;
     }
+    int count=0,index=0;
+    for(auto i =0 ; i<v.size(); i++)
+    {
+        if(m.find(v[i])!= m.end())
+          count++;
+        //  cout << v[i] << endl;
+        //index++;
+    }
 
 
 
 
+      //for(auto i=m.begin(); i!=m.end();++i)
+        //cout << i->first << " "<< i->second<< endl;;
 
-      for(auto i=m.begin(); i!=m.end();++i)
-        cout << i->first << " "<< i->second<< endl;;
-
-    return 0;
+    return count;
 }
 
 
 
 int main(int argc, char const *argv[]) {
   /* code */
-  string words [] ={"welcome", "to", "geeks", "portal"};
+   string words [] ={"welcome", "to", "geeks", "portal"};
+  std::vector<string> v(words, words+4);
   string str = "geeksforgeeks is a computer science portal for geeks.";
 
-  cout << " no of words prsent " << count_words(words, str) << endl;
+  cout << " no of words prsent " << count_words(v, str) << endl;
 
   return 0;
 }
