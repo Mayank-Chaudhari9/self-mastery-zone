eec74bcef2974694a66af27d730a3b5576454624
Mayank-Chaudhari9 <m.charly3015@gmail.com>
2017-07-02 10:21:09 +0530

    room commit

--- DP/rod_cutting.cpp
+++ DP/rod_cutting.cpp
@@ -30,11 +30,11 @@ int rodcut_dp(int price[], int n)
 	val[0]=0;
 	int i,j;
 
-	for(i=1;1<=n;i++)
+	for(i=1;i<=n;i++)
 	{
 		int max_val=INT_MIN;
 		for(j=0;j<i;j++)
-			max_val = max(max_val, price[j]+val[i-j-1]);
+			max_val =max(max_val, price[j]+val[i-j-1]);
 		val[i]=max_val;
 	}
 	return val[n];
