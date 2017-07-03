f3652ec42411d382286bb5db634029df43d5023a
Mayank-Chaudhari9 <m.charly3015@gmail.com>
2017-07-04 02:57:25 +0530

    day commit 0

--- array/sort_by_freq.cpp
+++ array/sort_by_freq.cpp
@@ -2,22 +2,42 @@
 
 using namespace std;
 
+auto cmp=[](pair<a, pair<b,c>>&x,pair<a, pair<b,c>>&y)
+{
+	return x.second.first> y.second.first;
+}
+
 void print(int a[], int len)
 {
 
 	//nt len= sizeof(a)/sizeof(a[0]);
-	unordered_map<int,int> m;
-	int i;
-	for(i=0; i<len; i++);
+	map<int,pair<int,int>> m;
+	//int i;
+
+	for(int i=0; i<len; i++)
 	{
-		m[a[i]]++;
+		int x=a[i];
+		//cout << a[i] << endl;
+		
+		if(m.find(x)==m.end())
+		{
+			m[x].first=i;
+			m[x].second++;
+		}
+		else
+			m[x].second++;
+
+
 
 	}
-	unordered_map<int, int> ::iterator it;
+
+	sort(m.begin(),m.end(),cmp);
+
+	map<int,pair<int,int>> ::iterator it;
 
 	for(it=m.begin();it!=m.end();++it)
 	{
-		cout << it->first <<" " <<it->second << endl;
+		cout << it->first <<" " <<it->second.first<< " "<< it->second.second<<endl;
 	}
 
 }
@@ -28,8 +48,8 @@ void print(int a[], int len)
 
 int main()
 {
-	int a[] = {5,2,3,2, 4, 5, 12, 2, 3, 3, 3, 12,4};
-	int len =sizeof(a)/sizeof(a[0]);
+	int a[]= {2,3,2,4,6,7,8,3,5,6,9,4,6,2,5,6};
+	int len = sizeof(a)/sizeof(int);
 	print(a, len);
 	return 0;
 }
