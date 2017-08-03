91211ad462666fa5ea8882cd3fb6135a2710dad8
Mayank-Chaudhari9 <m.charly3015@gmail.com>
2017-08-04 04:22:58 +0530

    level

---
+++ TREE/level_order.cpp
@@ -0,0 +1,71 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+struct node
+{
+  int data;
+  node *left, *right;
+};
+
+node *newnode(int data)
+{
+  node *temp= new node;
+  temp->data=data;
+  temp->left,temp->right=NULL;
+  return temp;
+}
+
+void print(node *root)
+{
+  if(root!=NULL)
+  {
+    print(root->left);
+    cout << root->data<<" ";
+    print(root->right);
+  }
+}
+
+void print_level(node *root)
+{
+  queue<node*>q;
+  q.push(root);
+  while(!q.empty())
+  {
+    node *val=q.front();
+    //q.pop();
+    //cout << "here" << endl;
+    cout<<val->data << " ";
+    if(val->left!=NULL)
+      q.push(val->left);
+    if(val->right!=NULL)
+      q.push(val->right);
+    q.pop();
+
+
+  }
+}
+
+int main()
+{
+  node *root=NULL;
+  root=newnode(10);
+  root->left=newnode(12);
+  root->right=newnode(15);
+
+  root->left->left=newnode(25);
+  root->left->right=newnode(30);
+  root->right->left=newnode(36);
+
+  cout << "inorder is\n";
+  print(root);
+
+  cout << endl;
+
+  cout << "level order is" <<endl;
+  print_level(root);
+
+  cout << endl;
+
+  return 0;
+}
