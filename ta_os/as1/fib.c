db162869c448be68b7acb8ef384821cc6ffc144c
Mayank-Chaudhari9 <m.charly3015@gmail.com>
2017-08-17 23:39:14 +0530

    Fibonnaci

--- ta_os/as1/fib.c
+++ ta_os/as1/fib.c
@@ -11,31 +11,45 @@ int main(int argc, char* argv[])
   int n=a+b;
   int num;
 
-
+ // check for valid no of arguements
   if(argc<=1 || argc>2)
   {
     printf("Enter correct no of arguement \n");
   }
 
+//check for valid input
   num = atoi(argv[1]);
-
   if(num < 0)
     printf("Invalid input \n");
 
 
-
-
-
+//Fibonnaci no using child
   pid_t pid = fork();
 
   if(pid==0)
   {
-    printf("Child is make the Fibonnaci\n", );
+    printf("Child process calculating Fibonnaci\n");
     while(num >0)
     {
-
+      n=a+b;
+      printf("%d\n",n);
+      a=b;
+      b=n;
+      num--;
+      if (num==0)
+      {
+        printf("Child processing exiting\n");
+      }
     }
   }
+
+  else
+  {
+    printf("Parent is waiting for child to complete\n");
+    waitpid(pid,NULL,0);
+    printf("Parent exiting \n");
+  }
+
 
   return 0;
 }
